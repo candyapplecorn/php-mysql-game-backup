69da81174277bea61343aff3311076dcaea21ea1
user <user@gordon@joeburger.ax.lt>
2015-11-27 16:04:00 -0500

    New repo, node.js version

---
+++ MySQL/commands.sql
@@ -0,0 +1,568 @@
+-- ============================================================
+--      SCHEMA CREATION 
+-- ============================================================
+create database if not exists html5game; 
+
+USE html5game; 
+
+CREATE TABLE IF NOT EXISTS players ( 
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+username VARCHAR(255) NOT NULL UNIQUE, 	-- DEFAULT 'defaultusername', 
+password VARCHAR(255) NOT NULL, 	-- DEFAULT '5f4dcc3b5aa765d61d8327deb882cf99', 
+email VARCHAR(255) NOT NULL UNIQUE,
+money INT DEFAULT 0 CHECK (MONEY >= 0),
+fuel INT DEFAULT 0 CHECK (FUEL >= 0),
+alliance VARCHAR(255) DEFAULT '',       -- For optional alliances (teams)
+rank INT DEFAULT 0,                     -- If we decide to add a level-up / rank system
+alerts INT DEFAULT 0, 
+lastlogin DATETIME DEFAULT '2015-01-01 00:00:00'
+);
+
+/*
+The whole game recolves around rows, so it should be a pretty detailed table!
+*/
+CREATE TABLE IF NOT EXISTS gamerows ( 
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+owner INT DEFAULT 0 REFERENCES players(id),
+ownerusername VARCHAR(255) DEFAULT '', 
+morale DOUBLE DEFAULT 0,
+defenders INT DEFAULT 0 CHECK (defenders >= 0),
+attackers INT DEFAULT 0 CHECK (attackers >= 0),
+money INT DEFAULT 0 CHECK (MONEY >= 0),
+fuel INT DEFAULT 0 CHECK (FUEL >= 0),
+mgs INT DEFAULT 0 CHECK (MGS >= 0),
+fgs INT DEFAULT 0 CHECK (FGS >= 0),
+dgs INT DEFAULT 0 CHECK (DGS >= 0),
+hospital INT DEFAULT 0 CHECK (hospital >= 0),
+hospital_level INT DEFAULT 0 CHECK (hospital_level >= 0),
+healed INT DEFAULT 0,
+period INT DEFAULT 0,
+attack_level INT DEFAULT 0,
+defense_level INT DEFAULT 0,
+losses INT DEFAULT 0,
+investments INT DEFAULT 0 CHECK (investments >= 0),
+investors VARCHAR(255) DEFAULT '', -- This will be a string containing a JSON object.
+lastattacked DATETIME,
+lastaccessed DATETIME
+);
+
+-- ============================================================
+--     PROCEDURES 
+-- ============================================================
+-- This is my first time making an actual function in pure sql! WOO HOO!
+delimiter //
+
+CREATE PROCEDURE fillgamerows(numrows INT)
+BEGIN
+	SET @x = 0;
+	REPEAT
+	INSERT INTO gamerows (lastattacked, lastaccessed) VALUES (NOW(), NOW());
+	SET @x = @x + 1;
+	UNTIL @x >= numrows END REPEAT;
+END
+//
+
+CREATE PROCEDURE grantrow(row_id INT, user_id INT)
+BEGIN
+	UPDATE gamerows
+	SET owner=user_id, 
+	ownerusername=(SELECT username FROM players WHERE id=user_id),
+	morale=100, defenders=DEFAULT(defenders), attackers=DEFAULT(attackers),
+	money=DEFAULT(money), fuel=DEFAULT(fuel), mgs=1, fgs=1,
+	dgs=DEFAULT(dgs), investments=DEFAULT(investments), investors='',
+	lastaccessed=NOW()
+	WHERE id=row_id;
+END
+//
+
+/* Perhaps it would be smart to add this to the buy_item proc */
+-- procedure to buy attackers
+delimiter //
+CREATE PROCEDURE purchase_attackers(row_id INT, num2buy INT)
+BEGIN
+DECLARE attacker_cost, cost_per_attacker INT DEFAULT 100;
+SET attacker_cost =  (SELECT ABS(num2buy * cost_per_attacker));
+UPDATE gamerows
+SET money = money - attacker_cost,
+attackers = attackers + num2buy
+WHERE id = row_id
+AND attacker_cost <= money;
+END;
+//
+
+/*
+Scan takes a row id and updates the corresponding row and the next
+9 corresponding rows.
+
+The parameter "display" is a TINYINT, which is what BOOLEAN is
+an alias for in MySQL, with 0 being false.
+
+DEVNOTE - Change income so that rather than "fuel_rate * fgs", 
+it's something more interesting like "POW(1.2, num_generators)"
+*/
+CREATE PROCEDURE scan(row_id INT, display TINYINT)
+BEGIN
+
+DECLARE fuel_rate, money_rate DOUBLE(2, 1) DEFAULT 1.2;
+
+UPDATE gamerows
+SET period = timestampdiff(second, lastaccessed, now()),
+healed = IF(timestampdiff(second, lastaccessed, now()) * (1 + hospital_level) > hospital, hospital, timestampdiff(second, lastaccessed, now()) * (1 + hospital_level))
+WHERE id >= row_id AND id <= row_id + 9 AND owner != 0;
+
+UPDATE gamerows
+SET fuel = fuel + (period * POW(fuel_rate, fgs)),
+money = money + (period * POW(money_rate, mgs)),
+hospital = hospital - healed,
+defenders = defenders + healed,
+morale = IF(morale >= 100, 100, morale + (period / 60)),
+lastaccessed = now()
+WHERE id >= row_id AND id <= row_id + 9 AND owner != 0;
+
+SELECT gamerows.ID, gamerows.ownerusername AS Owner, gamerows.defenders + gamerows.attackers AS Forces, gamerows.Money, gamerows.Fuel, ROUND(gamerows.morale) AS Morale
+FROM gamerows
+LEFT JOIN players
+ON players.id = gamerows.owner
+WHERE gamerows.id >= row_id
+AND gamerows.id <= row_id + 9
+AND display != 0;
+
+END
+//
+
+CREATE PROCEDURE debug_scan(row_id INT, display TINYINT)
+BEGIN
+CALL scan(row_id, 0);
+SELECT ID, ownerusername AS Owner, Attackers, Defenders, Money, Fuel, MGS, FGS, Hospital, hospital_level AS H_lvl, attack_level AS attack, defense_level AS defense
+FROM gamerows
+WHERE row_id = id
+AND display != 0;
+END
+//
+
+/*
+Buy a fuel or money generator
+row_id : the row purchasing the item,
+item: the type to buy
+0 - money generator, 1 - fuel generator, 2 - hospital level, 3 - attack level, 4 - defense level
+*/
+CREATE PROCEDURE purchase_item(row_id INT, item INT)
+this_proc: BEGIN
+DECLARE cost, hsptl_lvl, money_generators, fuel_generators, cash, defense, attack INT DEFAULT 0;
+DECLARE flag TINYINT DEFAULT 0;
+CALL scan(row_id, 0);
+SELECT mgs, fgs, money, hospital_level, attack_level, defense_level FROM gamerows WHERE id = row_id INTO money_generators, fuel_generators, cash, hsptl_lvl, attack, defense;
+
+CASE item
+WHEN 0 THEN
+SET cost = POW(2, money_generators);
+WHEN 1 THEN
+SET cost = POW(2, fuel_generators);
+WHEN 2 THEN
+SET cost = POW(2, hsptl_lvl);
+WHEN 3 THEN
+SET cost = 3600 * POW(2, attack);
+WHEN 4 THEN
+SET cost = (3600 / 2) * POW(2, defense);
+END CASE;
+
+UPDATE gamerows
+SET money = money - cost,
+mgs = mgs + IF(item = 0, 1, 0),
+fgs = fgs + IF(item = 1, 1, 0),
+hospital_level = hospital_level + IF(item = 2, 1, 0),
+attack_level = attack + IF(item = 3, 1, 0),
+defense_level = defense + IF(item = 4, 1, 0)
+WHERE id = row_id
+AND cost <= cash;
+
+END this_proc
+//
+
+/* this proc will need to be updated later */
+CREATE PROCEDURE show_costs(row_id INT)
+BEGIN
+SELECT POW(2, mgs) AS "Next Money", 
+POW(2, fgs) AS "Next Fuel",
+POW(2, hospital_level) AS "Next Hospital",
+3600 * POW(2, attack_level) AS "Next Attack",
+(3600 / 2) * POW(2, defense_level) AS "Next Defense"
+FROM gamerows
+WHERE id = row_id;
+END
+//
+
+/*
+We need a procedure to send resources from one row
+to another; attack already takes care of sending attackers
+*/
+CREATE PROCEDURE send_resources(source_row INT, destination_row INT, money_sent INT, fuel_sent INT)
+this_proc: BEGIN
+    DECLARE flag, row_owner, row_owner2 INT DEFAULT 0;
+    SELECT owner, IF(money >= money_sent AND fuel >= fuel_sent, 1, 0) FROM gamerows WHERE id = source_row INTO row_owner, flag;
+    SELECT owner FROM gamerows WHERE id = destination_row INTO row_owner2;
+
+    /* If the user tries to send resources to a row he/she doesn't own */
+    IF row_owner != row_owner2 THEN
+        LEAVE this_proc;
+    END IF;
+
+    UPDATE gamerows
+    SET money = money - money_sent, fuel = fuel - fuel_sent
+    WHERE id = source_row AND flag = 1;
+
+    UPDATE gamerows
+    SET money = money + money_sent, fuel = fuel + fuel_sent
+    WHERE id = destination_row AND flag = 1;
+
+END this_proc
+//
+
+/*
+We need a procedure to allow a player to start a new row
+Will use select count(owner) from gamerows where owner = source_row.owner
+*/
+CREATE PROCEDURE found_new_row (source_row INT, new_row INT)
+BEGIN
+    DECLARE cash, numrows, cost, userid INT;
+    SELECT POW(COUNT(owner), COUNT(owner)), owner, money
+    FROM gamerows as a
+    WHERE owner = (SELECT owner FROM gamerows WHERE id = source_row)
+    INTO cost, userid, cash;
+
+    IF cash >= cost AND (SELECT morale FROM gamerows WHERE id = new_row) < 1 THEN
+	    UPDATE gamerows
+	    SET money = money - cost
+        WHERE id = source_row;
+	    CALL grantrow(new_row, userid);  
+    END IF;
+END
+//
+
+/*
+If a row is destroyed it must be reset to default values.
+If attacking_row = 0, then we're not going to grant
+any loot to any attacking row, since there was none
+(Example: If a player wanted to delete his/her own row,
+attacking_row would be 0)
+*/
+CREATE PROCEDURE destroy_row (attacking_row INT, dying_row INT)
+BEGIN
+    DECLARE dm, df, dforces INT;
+    CREATE TEMPORARY TABLE IF NOT EXISTS deadrow (
+        SELECT * FROM gamerows WHERE id = dying_row
+    );
+    SELECT money, fuel, hospital + defenders + attackers
+    FROM deadrow
+    INTO dm, df, dforces;
+
+	UPDATE gamerows
+	SET owner=DEFAULT(owner), ownerusername=DEFAULT(ownerusername),
+	morale=DEFAULT(morale), defenders=DEFAULT(defenders), attackers=DEFAULT(attackers),
+	money=DEFAULT(money), fuel=DEFAULT(fuel), mgs=DEFAULT(mgs), fgs=DEFAULT(fgs),
+	dgs=DEFAULT(dgs), investments=DEFAULT(investments), investors=DEFAULT(investors),
+    hospital=DEFAULT(hospital), hospital_level=DEFAULT(hospital_level),
+    attack_level=DEFAULT(attack_level), defense_level=DEFAULT(defense_level),
+	lastaccessed=NOW()
+	WHERE id=dying_row;
+
+    /* give the victor some prizes! */
+    UPDATE gamerows
+    SET money = money + dm,
+    fuel = fuel + df,
+    attackers = attackers + dforces
+    WHERE id = attacking_row AND id != 0;
+
+    DROP TABLE IF EXISTS deadrow;
+END
+//
+
+/* helper procedure used for attack() */
+/* It first deducts from the defender's
+defenders, and then from the attackers if 
+need be. If the defender wins a defense battle
+then the defender might not need to lose their
+attackers, for example */
+CREATE PROCEDURE assign_losses(row_id INT, deduction INT)
+BEGIN
+    DECLARE num_defenders, num_attackers INT;
+    SELECT attackers, defenders
+    FROM gamerows
+    WHERE id = row_id
+    INTO num_attackers, num_defenders;
+
+    SET num_defenders = num_defenders - deduction;
+    IF num_defenders < 0 THEN
+        SET deduction = ABS(num_defenders);
+        SET num_defenders = 0;
+        SET num_attackers = num_attackers - deduction;
+        SET num_attackers = IF(num_attackers < 0, 0, num_attackers);
+    END IF;
+
+    UPDATE gamerows
+    SET defenders = num_defenders,
+    attackers = num_attackers
+    WHERE id = row_id;
+END
+//
+
+/*
+Attack a row
+If owner of source_row is same as owner of destination_row, 
+just transfer units instead
+
+I realize the function is really ugly, I might rewrite it
+eventually. For now it gets the job done. There might be 
+bugs.
+*/
+CREATE PROCEDURE attack(source_row INT, destination_row INT, attackers_sent INT)
+this_proc: BEGIN
+    DECLARE dmoney, dfuel, money_loot, fuel_loot, attacking_player, defending_player, att_lvl, def_lvl, attacker_home_forces, defending_forces, attacker_fuel, fuel_cost, distance INT;
+    DECLARE AF, DF, CF DOUBLE DEFAULT 0;
+    SET attackers_sent = abs(attackers_sent), source_row = abs(source_row), destination_row = abs(destination_row);
+    SELECT ABS(source_row - destination_row) INTO distance;
+    SELECT gamerows.fuel, gamerows.attackers, gamerows.owner, gamerows.attack_level FROM gamerows WHERE id = source_row  INTO attacker_fuel, attacker_home_forces, attacking_player, att_lvl;
+    SELECT owner, attackers + defenders, defense_level, money, fuel FROM gamerows WHERE id = destination_row INTO defending_player, defending_forces, def_lvl, dmoney, dfuel;
+    SELECT distance * attackers_sent INTO fuel_cost;
+
+    /* Update the two fighting rows */
+    CALL scan(source_row, 0);
+    CALL scan(destination_row, 0);
+
+IF fuel_cost > attacker_fuel OR attackers_sent > attacker_home_forces OR attacking_player < 1 THEN
+    LEAVE this_proc;
+END IF;
+
+/* The player is transferring troops */
+IF attacking_player = defending_player THEN
+    UPDATE gamerows as g
+    SET g.attackers = g.attackers - attackers_sent,
+    g.fuel = g.fuel - fuel_cost
+    WHERE id = source_row;
+    UPDATE gamerows as g
+    SET g.attackers = g.attackers + attackers_sent
+    WHERE id = destination_row;
+
+/* The player is attacking an empty row */
+ELSEIF defending_player < 1 THEN
+    select "There's nobody here!";
+
+/* The player is attacking another player! */
+ELSEIF attacking_player != defending_player AND defending_player > 0 THEN
+    SET AF = attackers_sent * POW(1.2, att_lvl);
+    SET DF = defending_forces * POW(1.2, def_lvl);
+    SET CF = AF - DF;
+
+    /* Victory for the attacker */
+    IF CF >= 0 THEN
+        SET AF = CF,
+        money_loot = IF(AF * 50 > dmoney, dmoney, AF * 50), 
+        fuel_loot = IF(AF * 50 > dfuel, dfuel, AF * 50);
+        
+        /* update the defender */
+        CALL assign_losses(destination_row, DF / POW(1.2, def_lvl) );
+
+        UPDATE gamerows
+        SET money = money - money_loot,
+        fuel = fuel - fuel_loot,
+        hospital = hospital + (DF / POW(1.2, def_lvl) * 7 / 12),
+        defenders = defenders + (DF / POW(1.2, def_lvl) * 1 / 12),
+        losses = losses + (DF / POW(1.2, def_lvl) ),
+        morale = morale - 10
+        WHERE id = destination_row;
+
+        /* update the attackers */
+        UPDATE gamerows
+        SET money = money + money_loot,
+        fuel = fuel + fuel_loot,
+        attackers = attackers - ( 2 / 3 * (DF / POW(1.2, def_lvl)) ),
+        morale = morale + IF(morale < 90, RAND() * 5, 0)
+        WHERE id = source_row;
+
+        /* if the defender's row has its morale brought below 1, destroy it. */
+        IF (SELECT morale FROM gamerows WHERE id = destination_row) < 1 THEN
+            CALL destroy_row(source_row, destination_row);
+        END IF;
+
+    /* The attacker lost the battle */
+    ELSEIF CF < 0 THEN
+        /* The attacker has been defeated so doesn't receive any loot */
+        /* Nor does the attacking player recieve a cut of the losses */
+        SET CF = ABS(CF);
+        CALL assign_losses(destination_row, AF / POW(1.2, def_lvl) );
+
+        UPDATE gamerows
+        SET hospital = hospital + (AF / POW(1.2, def_lvl) * 7 / 12),
+        defenders = defenders + (AF / POW(1.2, def_lvl) * 1 / 12),
+        losses = losses + (AF / POW(1.2, def_lvl) )
+        WHERE id = destination_row;
+
+        UPDATE gamerows
+        SET attackers = attackers - attackers_sent,
+        morale = morale - RAND() * 5 
+        WHERE id = source_row;
+    END IF;
+END IF;
+
+END this_proc
+//
+
+-- ============================================================
+--     USER MANAGEMENT 
+-- ============================================================
+
+CREATE PROCEDURE add_user(usern VARCHAR(255), passw VARCHAR(255), em VARCHAR(255))
+BEGIN
+    DECLARE newuserid, newuserfirstrow INT DEFAULT 0;
+    DECLARE EXIT HANDLER FOR 1062 SELECT "username taken!";
+INSERT INTO players (username, password, email)
+VALUES (usern, (select md5(passw)), em);
+
+WHILE newuserfirstrow = 0 OR newuserfirstrow > (SELECT COUNT(id) FROM gamerows) DO
+    SELECT find_new_user_first_row() INTO newuserfirstrow;
+    IF newuserfirstrow = 0 OR newuserfirstrow > (SELECT COUNT(id) FROM gamerows) THEN
+        SELECT "Calling fillgamerows";
+        CALL fillgamerows(10);
+    END IF;
+END WHILE;
+SELECT id FROM players WHERE username = usern INTO newuserid;
+
+CALL grantrow(newuserfirstrow, newuserid);
+END
+//
+
+CREATE FUNCTION find_new_user_first_row()
+RETURNS INT
+BEGIN
+DECLARE num, max, found, curr, step INT;
+SET curr = 0, step = 10, found = 0;
+SELECT COUNT(ID) FROM gamerows INTO max;
+
+WHILE found = 0 AND step > step - 2 DO
+
+    WHILE found = 0 AND found < max DO
+        SELECT count(owner) FROM gamerows
+        WHERE id >= curr AND id <= curr + step AND owner != 0
+        INTO num;
+
+        /* We've found a spot for the player! */
+        IF num <= 1 THEN
+            SET found = curr + ROUND(RAND() * step);
+            WHILE (SELECT id FROM gamerows WHERE owner != 0 AND id >= curr AND id <= curr + step) = found DO
+                SET found = curr + ROUND(RAND() * step);
+            END WHILE;
+        END IF;
+
+        SET curr = curr + step;
+    END WHILE;
+    /* check to see if we couldn't find a spot */
+    IF found = 0 THEN
+        SET step = step - 1;
+    END IF;
+
+END WHILE;
+
+IF found = 0 THEN
+    SET found = found;/*SELECT "You need to add more rows.";*/
+END IF;
+
+RETURN found;
+END
+//
+
+CREATE PROCEDURE remove_user(usern VARCHAR(255))
+BEGIN
+DELETE FROM players
+WHERE username = usern;
+END
+//
+
+CREATE PROCEDURE login(usern VARCHAR(255), passw VARCHAR(255))
+BEGIN
+    DECLARE idnum INT DEFAULT 0;
+
+    SELECT id
+    FROM players
+    WHERE username = usern
+    AND password = md5(passw)
+    INTO idnum;
+
+    /* If the user was found */
+    IF idnum != 0 THEN
+	    UPDATE players
+	    SET lastlogin = NOW()
+	    WHERE id = idnum;
+
+	    SELECT "User found!";
+    ELSE
+        SELECT CONCAT("Incorrect password, ", username, ".") AS Username
+        FROM players
+        WHERE usern = username;
+    END IF;
+
+    /*SELECT CONCAT("User ", usern, " isn't registered!") AS Errmsg
+    FROM players
+    WHERE usern NOT IN players;*/
+END
+//
+
+CREATE PROCEDURE set_alerts(usern VARCHAR(255), switch TINYINT)
+BEGIN
+UPDATE players
+SET alerts = IF(switch != 0, 1, 0)
+WHERE username = usern;
+END
+//
+
+delimiter ;
+
+-- ============================================================
+--      DATABASE INITIALIZATION 
+-- ============================================================
+
+/*
+Now that we've entered our two main tables,
+let's populate them with some values.
+*/
+
+-- Let's make some rows in gamerows. 
+CALL fillgamerows(30);
+
+-- Now, we'll insert three players.
+call add_user("Alex", "password", "alex@alex.ru");
+call add_user("Joe", "betterpassword", "joe@joe.org");
+call add_user("Nick", "bestpassword", "nick@nick.net");
+
+/*
+-- Now, let's give them some rows!
+-- Give three rows to Alex
+CALL grantrow(5, 1);
+CALL grantrow(6, 1);
+CALL grantrow(7, 1);
+
+-- Give four rows to Nick
+CALL grantrow(9, 3);
+CALL grantrow(10, 3);
+CALL grantrow(11, 3);
+CALL grantrow(12, 4);
+
+-- Joe's a jerk and captures two of Nick's rows. HAHA NICK
+CALL grantrow(12, 2);
+CALL grantrow(11, 2);
+CALL grantrow(2, 2);
+
+UPDATE gamerows
+SET fuel = 1000,
+attackers = 100
+WHERE id = 2;
+
+UPDATE gamerows
+SET attackers = 50,
+money = 3000,
+fuel = 3000
+WHERE id = 5;
+
+UPDATE gamerows
+SET attackers = 200,
+money = 20000
+WHERE id = 6;*/
---
+++ MySQL/serverSetup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# This is a setup script by Joseph Burger
+# It gets the credentials to log into a mysql database,
+# and then runs a bunch of commands in a sql file.
+# To run this script/program: ($ means you enter the command
+# on a command line):
+# $bash serverSetup.sh OPTION
+
+# So obviously you need to set up your mysql database first
+# so that you can use it over a command line. IDK if windows
+# lets you do this... Anyways, look it up on google, but
+# you should, by default, be able to log in as html5game. or
+# something. Mysql always comes with a default login you
+# can use.
+
+# Read the credentials into variables
+# Credentials is a file whose first line is the username,
+# and second line is the password to log in to the db
+
+cd MySQL/
+un=`find . -type f -name "credentials.ini" -exec sed -En 's/user.*\s(\w+);/\1/ p' {} \;`
+pw=`find . -type f -name "credentials.ini" -exec sed -En 's/password.*\s(\w+);/\1/ p' {} \;`
+
+if [[ $# -eq 1 && $1 =~ "delete" || $1 =~ "d" ]];
+then
+    echo "Deleting the html5 game database."
+    mysql -u$un -p$pw -e "drop database html5game;"
+elif [[ $# -eq 1 && $1 =~ "create" || $1 =~ "c" ]];
+then
+    echo "Creating the html5 game database."
+    mysql -u$un -p$pw < commands.sql
+    echo "html5 game database created."
+elif [[ $# -eq 1 && $1 =~ "remake" || $1 =~ "r" ]];
+then
+    echo "Deleting the html5 game database."
+    mysql -u$un -p$pw -e "drop database html5game;"
+    echo "Creating the html5 game database."
+    mysql -u$un -p$pw < commands.sql
+    echo "html5 game database created."
+else
+    echo "usage: serverSetup.sh [c(reate)|d(elete)|r(emake)]";
+fi;
+
+# Example of a credentials file:
+# Ignore the comments and line numbers...
+#line 1 Alex
+#line 2 Password123
+
+# just get rid of the hashpounds, those are comments in bash.
+# so without "line 1/2" your credentials would be
+
+#Alex
+#Password123
